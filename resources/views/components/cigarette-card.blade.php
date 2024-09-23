850acadbb5d67934cd968fe93cbceef5f13276f1
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2024-09-23 12:41:48 +0200

    added code for cigarette-card & liquid-card

--- resources/views/components/cigarette-card.blade.php
+++ resources/views/components/cigarette-card.blade.php
@@ -1,3 +1,34 @@
+@props(['name' => '#', 'image' => '#', 'strength' => '#', 'puffs' => '#', 'flavor' => '#', 'price' => '#' ])
+
+<li class="grid__item">
+    <article class="product elfbar">
+        {{-- Image --}}
+        <img class="product__image" src="{{ asset('./storage/images/' . $image) }}" alt="{{ $name }}" />
+        <div class="product__description">
+            {{-- Name --}}
+            <h2 class="product__name">{{ $name }}</h2>
+            {{-- Info --}}
+            <div class="product__info">
+                ✔&nbsp;Moc - {{ $strength }}%
+                <br>
+                ✔&nbsp;{{ $puffs }} zaciągnięć
+                <br>
+                ✔&nbsp;Smak: {{ $flavor }}
+
+            </div>
+            {{-- Price --}}
+            <p class="product__price">Cena: {{ $price }} zł</p>
+        </div>
+        <button class="button product__buy">Kupić</button>
+        {{-- Cart button --}}
+        <button type="button" class="button product__button" aria-label="Dodaj do koszyka">
+            <svg class="button__icon" width="20" height="20">
+                <use href="./storage/images/footer-icons/symbol-defs.svg#icon-shopping-cart"></use>
+            </svg>
+        </button>
+    </article>
+</li>
+
 <div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
+    {{ $slot }}
 </div>
--- resources/views/components/liquid-card.blade.php
+++ resources/views/components/liquid-card.blade.php
@@ -1,3 +1,28 @@
+@props(['name' => '#', 'image' => '#', 'pg_vg_ratio' => '#', 'volume' => '#', 'flavor' => '#', 'price' => '#'])
+
+<li class="grid__item">
+    <article class="product liquid">
+        <img class="product__image" src="{{ asset('./storage/images/liquid/' . $image) }}" alt="{{ $name }}" />
+        <div class="product__description">
+            <h2 class="product__name">{{ $name }}</h2>
+            <div class="product__info">
+                ✔&nbsp;PG/VG - {{ $pg_vg_ratio }}
+                <br>
+                ✔&nbsp;Pojemność - {{ $volume }} ml
+                <br>
+                ✔&nbsp;Smak: {{ $flavor }}
+            </div>
+            <p class="product__price">Cena: {{ $price }} zł</p>
+        </div>
+        <button class="button product__buy">Kupić</button>
+        <button type="button" class="button product__button" aria-label="Dodaj do koszyka">
+            <svg class="button__icon" width="20" height="20">
+                <use href="./storage/images/footer-icons/symbol-defs.svg#icon-shopping-cart"></use>
+            </svg>
+        </button>
+    </article>
+</li>
+
 <div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
+    {{ $slot }}
 </div>
