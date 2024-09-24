8a910e890b39661c2cb3c7f02165574e7d1bf051
sammri <ivildemort4837@gmail.com>
2024-09-24 16:56:20 +0300

    added carousele to a index.blade.html

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -343,7 +343,76 @@
                 <img class="product__image" src="{{ asset('./storage/images/reviews-img/tg1.jpg') }}" alt="Liquid Mango" />
 
         </div> -->
-bbr
+<section class="reviews">
+            <h2 class="reviews-title">RECENZJE</h2>
+            <div class="star__img">
+            <svg role="presentation" width="" height="21.48px" viewBox="0 0 88 21" class="star__svg">
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g fill="#ff5100">
+                        <path
+                            d="M77.2,20.9 C77.2,20.9 76.3,16.6 73.7,14 C71.1,11.4 66.8,10.5 66.8,10.5 C66.8,10.5 71.1,9.6 73.7,7 C76.3,4.3 77.2,0 77.2,0 C77.2,0 78.1,4.3 80.7,7 C83.3,9.6 87.7,10.5 87.7,10.5 C87.7,10.5 83.4,11.4 80.8,14 C78.1,16.6 77.2,20.9 77.2,20.9 L77.2,20.9 Z"
+                            id="shape"></path>
+                        <path
+                            d="M44.3,20.9 C44.3,20.9 43.4,16.6 40.8,14 C38.2,11.4 33.9,10.5 33.9,10.5 C33.9,10.5 38.2,9.6 40.8,7 C43.4,4.3 44.3,0 44.3,0 C44.3,0 45.2,4.3 47.8,7 C50.4,9.6 54.8,10.5 54.8,10.5 C54.8,10.5 50.5,11.4 47.9,14 C45.2,16.6 44.3,20.9 44.3,20.9 L44.3,20.9 Z"
+                            id="shape"></path>
+                        <path
+                            d="M11.1,20.9 C11.1,20.9 10.2,16.6 7.6,14 C5,11.4 0.7,10.5 0.7,10.5 C0.7,10.5 5,9.6 7.6,7 C10.1,4.3 11.1,0 11.1,0 C11.1,0 12,4.3 14.6,7 C17.2,9.6 21.6,10.5 21.6,10.5 C21.6,10.5 17.3,11.4 14.7,14 C12,16.6 11.1,20.9 11.1,20.9 L11.1,20.9 Z"
+                            id="shape">
+                        </path>
+                    </g>
+                </g>
+            </svg>
+
+
+        </div>
+        <div class="carousel">
+    <div class="carousel-item">
+        <div class="carousel-box">
+            <img class="product__image" src="{{ asset('./storage/images/reviews-img/tg1.jpg') }}" alt="Liquid Mango" />
+        </div>
+    </div>
+    
+    <div class="carousel-item">
+        <div class="carousel-box">
+            <img class="product__image" src="{{ asset('./storage/images/reviews-img/tg1.jpg') }}" alt="Liquid Mango" />
+        </div>
+    </div>  
+
+    <div class="carousel-item">
+        <div class="carousel-box">
+            <img class="product__image" src="{{ asset('./storage/images/reviews-img/tg1.jpg') }}" alt="Liquid Mango" />
+        </div>
+    </div>  
+
+    <div class="carousel-item">
+        <div class="carousel-box">
+            <img class="product__image" src="{{ asset('./storage/images/reviews-img/tg1.jpg') }}" alt="Liquid Mango" />
+        </div>
+    </div>  
+
+    <div class="carousel-item">
+        <div class="carousel-box">
+            <img class="product__image" src="{{ asset('./storage/images/reviews-img/tg1.jpg') }}" alt="Liquid Mango" />
+        </div>
+    </div>  
+    
+    <div class="carousel-item">
+        <div class="carousel-box">
+            <img class="product__image" src="{{ asset('./storage/images/reviews-img/tg1.jpg') }}" alt="Liquid Mango" />
+        </div>
+    </div>    
+    <div class="carousel-item">
+        <div class="carousel-box">
+            <img class="product__image" src="{{ asset('./storage/images/reviews-img/tg1.jpg') }}" alt="Liquid Mango" />
+        </div>
+    </div>    
+    <div class="carousel-item">
+        <div class="carousel-box">
+            <img class="product__image" src="{{ asset('./storage/images/reviews-img/tg1.jpg') }}" alt="Liquid Mango" />
+        </div>
+    </div>    
+</div>
+        </section>
 
 
 
