7772c1fa5eb167fc2b77cd9053d45273c668b5f8
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2024-10-04 20:06:58 +0200

    added @guest & @auth directory
    Now the buttons for product editing will be visible to admins only on the admin panel, the main page will display the usual buttons, which will also be seen by regular users

--- resources/views/components/cigarette-card.blade.php
+++ resources/views/components/cigarette-card.blade.php
@@ -19,6 +19,8 @@
             {{-- Price --}}
             <p class="product__price">Cena: {{ $price }} zł</p>
         </div>
+
+        @guest
         {{-- Buy button --}}
         <button class="button product__buy">Kupić</button>
         {{-- Cart button --}}
@@ -27,6 +29,26 @@
                 <use href="./storage/images/footer-icons/symbol-defs.svg#icon-shopping-cart"></use>
             </svg>
         </button>
+        @endguest
+
+        @auth
+            {{-- If admin is on admin_panel, display buttons for redact --}}
+            @if (Request::route()->getName() === 'admin_panel')
+                <button>Delete</button>
+                <button>Edit</button>
+                <button>Show</button>
+            {{-- If admin is on another page (index page), display buttons 'buy' and 'cart_button' --}}
+            @else
+                {{-- Buy button --}}
+                <button class="button product__buy">Kupić</button>
+                {{-- Cart button --}}
+                <button type="button" class="button product__button" aria-label="Dodaj do koszyka">
+                    <svg class="button__icon" width="20" height="20">
+                        <use href="./storage/images/footer-icons/symbol-defs.svg#icon-shopping-cart"></use>
+                    </svg>
+                </button>
+           @endif
+        @endauth
     </article>
 </div>
 
