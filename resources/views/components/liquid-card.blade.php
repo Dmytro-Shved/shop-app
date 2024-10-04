7ae2c53961b274dd7cd8ae1289fb194b545c3b4e
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2024-10-04 20:18:37 +0200

    added @auth & @guest durective for liquid-card
    same functionality like in cigarette card

--- resources/views/components/liquid-card.blade.php
+++ resources/views/components/liquid-card.blade.php
@@ -18,14 +18,36 @@
             {{-- Price --}}
             <p class="product__price">Cena: {{ $price }} zł</p>
         </div>
-        {{-- Buy button --}}
-        <button class="button product__buy">Kupić</button>
-        {{-- Cart button --}}
-        <button type="button" class="button product__button" aria-label="Dodaj do koszyka">
-            <svg class="button__icon" width="20" height="20">
-                <use href="./storage/images/footer-icons/symbol-defs.svg#icon-shopping-cart"></use>
-            </svg>
-        </button>
+        @guest
+            {{-- Buy button --}}
+            <button class="button product__buy">Kupić</button>
+            {{-- Cart button --}}
+            <button type="button" class="button product__button" aria-label="Dodaj do koszyka">
+                <svg class="button__icon" width="20" height="20">
+                    <use href="./storage/images/footer-icons/symbol-defs.svg#icon-shopping-cart"></use>
+                </svg>
+            </button>
+        @endguest
+
+        @auth
+            {{-- If admin is on admin_panel, display buttons for redact --}}
+            @if (Request::route()->getName() === 'admin_panel')
+                <button>Delete</button>
+                <button>Edit</button>
+                <button>Show</button>
+                {{-- If admin is on another page (index page), display buttons 'buy' and 'cart_button' --}}
+            @else
+                {{-- Buy button --}}
+                <button class="button product__buy">Kupić</button>
+                {{-- Cart button --}}
+                <button type="button" class="button product__button" aria-label="Dodaj do koszyka">
+                    <svg class="button__icon" width="20" height="20">
+                        <use href="./storage/images/footer-icons/symbol-defs.svg#icon-shopping-cart"></use>
+                    </svg>
+                </button>
+            @endif
+        @endauth
+
     </article>
 </div>
 
