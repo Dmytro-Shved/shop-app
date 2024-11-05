792aa4466da2c2b067c0a59da32ee615a086f560
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2024-11-05 08:47:22 +0100

    added livewire cart for admin & checkpoint before creating livewire modal cart

--- resources/views/livewire/cigarette-card.blade.php
+++ resources/views/livewire/cigarette-card.blade.php
@@ -49,58 +49,15 @@
                 </button>
 
                 {{-- If admin is on show page then don't display buttons --}}
-            @elseif(Request::route()->getName() === 'cigarettes.show')
+                @elseif(Request::route()->getName() === 'cigarettes.show')
 
-                {{-- If admin is on another page (index page), display buttons 'buy' and 'cart_button' --}}
+            {{-- If admin is on another page (index page), display buttons 'buy' and 'cart_button' --}}
             @else
                 {{-- Buy button --}}
                 <button class="button product__buy">Kupić</button>
 
-                {{-- If product is in the cart then show message and delete button --}}
-                @if(2 > 4)
-                    <div class="#">
-                        In cart
-                    </div>
-
-                    {{-- Delete from cart --}}
-                    <form action="{{ route('cart.delete') }}" method="POST">
-                        @csrf
-                        <!-- rowId -->
-                        <input type="hidden" name="rowId" value="">
-                        <!-- Product id -->
-                        <input type="hidden" name="product_id" value="{{ $cigarette->id }}">
-                        <!-- Type -->
-                        <input type="hidden" name="type" value="cigarette">
-
-                        <!-- Delete from cart button -->
-                        <button type="submit" class="#">Delete from cart</button>
-                    </form>
-
-                    {{-- If product in NOT in the cart, then show cart add button --}}
-                @else
-                    {{-- Cart button --}}
-                    <form action="{{ route('cart.store') }}" method="POST">
-                        @csrf
-
-                        {{-- Quantity control --}}
-                        <div class="quantity-control">
-                            <button type="button" class="quantity-button decrease">-</button>
-                            <input type="number" name="quantity" class="quantity-input" value="1" min="1" />
-                            <button type="button" class="quantity-button increase">+</button>
-                        </div>
-
-                        {{-- Product id --}}
-                        <input type="hidden" name="product_id" value="{{ $cigarette->id }}">
-                        <input type="hidden" name="type" value="cigarette">
-
-                        {{-- Cart button submit (with an image) --}}
-                        <button type="submit" class="button product__button" aria-label="Dodaj do koszyka">
-                            <svg class="button__icon" width="20" height="20">
-                                <use href="./storage/images/footer-icons/symbol-defs.svg#icon-shopping-cart"></use>
-                            </svg>
-                        </button>
-                    </form>
-                @endif
+                {{-- Cart buttons--}}
+                <livewire:cart :productId="$cigarette->id" :productName="$cigarette->name" productType="cigarette"/>
             @endif
         @endauth
     </article>
