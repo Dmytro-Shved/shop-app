c3d4105aaf1e019e48b5654f435e08ad459688d4
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2024-12-18 19:55:02 +0100

    added styles for cigarette-show & liquid-show

--- resources/views/product/cigarette-show.blade.php
+++ resources/views/product/cigarette-show.blade.php
@@ -5,34 +5,41 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/png" href="{{ asset('https://cdn-icons-png.flaticon.com/128/829/829117.png') }}">
+    @vite('resources/css/admin-show.css')
+
     <title>Show Panel</title>
 </head>
 
-{{-- @vite('resources/css/main.css')--}}
-
 <body>
-    {{-- Redirect to admin panel --}}
-    <button class="button button__back">
-        <a href="{{ route('admin_panel') }}">&larr; Go back to admin panel</a>
-    </button>
+{{-- Redirect to admin panel --}}
+<button class="button button__back">
+    <a href="{{ route('admin_panel') }}">&larr; Go back to admin panel</a>
+</button>
 
-    <div class="product__description" data-id="{{ $cigarette->id }}">
-        {{-- Name --}}
-        <h2 class="product__name">{{ $cigarette->name }}</h2>
-        {{-- Info --}}
-        <div class="product__info">
-            ✔&nbsp;Moc - {{ $cigarette->strength }}%
-            <br>
-            ✔&nbsp;{{ $cigarette->puffs }} zaciągnięć
-            <br>
-            ✔&nbsp;Smak: {{ $cigarette->flavor }}
-        </div>
-        {{-- Price --}}
-        <p class="product__price">Cena: {{ $cigarette->price }} zł</p>
-    </div>
-
+<section class="product-card">
     {{-- Image --}}
     <img class="product__image" src="{{ asset('./storage/' . $cigarette->image) }}" alt="{{ $cigarette->name }}" />
 
+    <div class="product__details">
+        {{-- Name --}}
+        <h2 class="product__name">{{ $cigarette->name }}</h2>
+
+        {{-- Price --}}
+        <p class="product__price">Cena: {{ $cigarette->price }} zł</p>
+
+        {{-- Info --}}
+        <div class="product__info">
+            <p><strong>Moc:</strong> {{ $cigarette->strength }}%</p>
+            <p><strong>Zaciągnięcia:</strong> {{ $cigarette->puffs }} zaciągnięć</p>
+            <p><strong>Smak:</strong> {{ $cigarette->flavor }}</p>
+        </div>
+    </div>
+</section>
+
+<footer class="footer">
+    <div class="footer-container">
+        <p>© 2024 ElfVapeShop. All rights reserved.</p>
+    </div>
+</footer>
 </body>
 </html>
--- resources/views/product/liquid-show.blade.php
+++ resources/views/product/liquid-show.blade.php
@@ -6,34 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/png" href="{{ asset('https://cdn-icons-png.flaticon.com/128/829/829117.png') }}">
     <title>Show Panel</title>
+    @vite('resources/css/admin-show.css')
 </head>
 
-{{--@vite('resources/css/main.css')--}}
-
 <body>
-    {{-- Redirect to admin panel --}}
-    <button class="button button__back">
-        <a href="{{ route('admin_panel') }}">&larr; Go back to admin panel</a>
-    </button>
+{{-- Redirect to admin panel --}}
+<button class="button button__back">
+    <a href="{{ route('admin_panel') }}">&larr; Go back to admin panel</a>
+</button>
 
+<section class="product-card">
     {{-- Image --}}
-    <div class="product__description" data-id="{{ $liquid->id }}">
+    <img class="product__image" src="{{ asset('./storage/' . $liquid->image) }}" alt="{{ $liquid->name }}" />
+
+    <div class="product__details">
         {{-- Name --}}
         <h2 class="product__name">{{ $liquid->name }}</h2>
-        {{-- Info --}}
-        <div class="product__info">
-            ✔&nbsp;PG/VG - {{ $liquid->pg_vg_ratio }}
-            <br>
-            ✔&nbsp;Pojemność - {{ $liquid->volume }} ml
-            <br>
-            ✔&nbsp;Smak: {{ $liquid->flavor }}
-        </div>
+
         {{-- Price --}}
         <p class="product__price">Cena: {{ $liquid->price }} zł</p>
+
+        {{-- Info --}}
+        <div class="product__info">
+            <p><strong>PG/VG:</strong> {{ $liquid->pg_vg_ratio }}</p>
+            <p><strong>Pojemność:</strong> {{ $liquid->volume }} ml</p>
+            <p><strong>Smak:</strong> {{ $liquid->flavor }}</p>
+        </div>
     </div>
+</section>
 
-    {{-- Image --}}
-    <img class="product__image" src="{{ asset('./storage/'. $liquid->image) }}" alt="{{ $liquid->name }}" />
-
+<footer class="footer">
+    <div class="footer-container">
+        <p>© 2024 ElfVapeShop. All rights reserved.</p>
+    </div>
+</footer>
 </body>
 </html>
