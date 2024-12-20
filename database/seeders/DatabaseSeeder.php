ec2e5f2fdcb36d8bb6a281e1c12f1a33b95dc794
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2024-12-20 07:36:15 +0100

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Admin;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Cigarette;
+use App\Models\Liquid;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,9 +17,39 @@ class DatabaseSeeder extends Seeder
     {
         // Admin::factory(10)->create();
 
-        Admin::factory()->create([
-            'name' => 'Test Admin',
-            'email' => 'test@example.com',
-        ]);
+//        Admin::factory()->create([
+//            'name' => 'Test Admin',
+//            'email' => 'test@example.com',
+//        ]);
+
+//        Cigarette::factory(1)->create([
+//            'id' => '1',
+//            'name' => 'Test Pod',
+//            'type' => 'pod',
+//            'strength' => '100',
+//            'puffs' => 1000,
+//            'flavor' => 'Apple Arctic Fresh',
+//            'price' => 120,
+//            'image' =>  'images/pod/xros.png',
+//        ]);
+//
+//        Cigarette::factory(1)->create([
+//            'name' => 'Test',
+//            'type' => 'elfbar',
+//            'strength' => '100',
+//            'puffs' => 1000,
+//            'flavor' => 'Juice',
+//            'price' => 110,
+//            'image' =>  'images/elfbar/blast.png',
+//        ]);
+
+//        Liquid::factory(1)->create([
+//            'name' => 'Test Liquid',
+//            'pg_vg_ratio' => '50/50',
+//            'volume' => 10,
+//            'flavor' => 'Arctic Fresh Mint',
+//            'price' => 25,
+//            'image' => 'images/liquid/VksOeSzFghNk0AvlgPSlEgN4eheu3HOzUudjGBkh.jpg',
+//        ]);
     }
 }
